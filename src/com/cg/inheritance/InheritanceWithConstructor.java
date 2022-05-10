13ff49d15e53ca4f4919e3e4f2616b8d393938d2
Gupta <shalu.a.gupta@capgemini.com>
2022-05-10 15:05:52 +0530

    1st commit

---
+++ src/com/cg/inheritance/InheritanceWithConstructor.java
@@ -0,0 +1,49 @@
+package com.cg.inheritance;
+
+class Base{
+	int x;
+	Base(){
+		System.out.println("Its Base class");
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+}
+
+class Derived{
+	int y;
+	Derived(){
+		super();
+		 System.out.println("hi");
+	}
+	Derived (int y){
+		super();
+		System.out.println("Y "+y);
+	}
+	
+	
+   
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
+public class InheritanceWithConstructor {
+
+	public static void main(String[] args) {
+		Derived d = new Derived(8);
+		Base b = new Base();
+		
+
+	}
+
+}
